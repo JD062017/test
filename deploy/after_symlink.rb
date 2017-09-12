3e691b4a68f8c6238ad8c8b6a009c4bd3cf6641e
Jaydeep Shome <jaydeep@Jaydeeps-MacBook-Air.local>
2017-09-13 02:28:38 +0530

    t6

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -1,3 +1,3 @@
-run "cp -pvr #{config.current_path}/_assets/ /efs/_assets/"
+run "cp -pvr #{config.current_path}/_assets/ /efs/"
 run "rm -Rf #{config.current_path}/_assets"
 #run "ln -nfs  /efs/_assets #{config.current_path}/_assets"
